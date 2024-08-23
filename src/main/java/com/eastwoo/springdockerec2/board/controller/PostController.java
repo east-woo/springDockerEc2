a1900b20ed2d51fdb0f53de27d2451037fda98f3
dongwoo <dwseo96@naver.com>
2024-08-23 19:41:12 +0900

    ResourceNotFoundException 정의, createPost,getPostById, getAllPosts, updatePost, deletePost 작성

---
+++ src/main/java/com/eastwoo/springdockerec2/board/controller/PostController.java
@@ -0,0 +1,8 @@
+package com.eastwoo.springdockerec2.board.controller;/**
+ * Please explain the class!!
+ *
+ * @fileName      : PostController
+ * @author        : dongwoo
+ * @since         : 2024-08-24
+ */public class PostController {
+}
---
+++ src/main/java/com/eastwoo/springdockerec2/board/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.eastwoo.springdockerec2.board.exception;/**
+ * Please explain the class!!
+ *
+ * @fileName      : ResourceNotFoundException
+ * @author        : dongwoo
+ * @since         : 2024-08-24
+ */public class ResourceNotFoundException {
+}
